19a1b417fb3137bb667baaeb49b69dceeba4608a
Paun Wander <tsme0613@gmail.com>
2024-11-16 22:15:16 +0800

    First commit

---
+++ backend/utils.py
@@ -0,0 +1,22 @@
+import json
+from PIL import Image
+from supabase import create_client, Client
+import readWrite as rw
+from datetime import datetime
+
+# Load URL and Key from JSON file
+with open('RasPi/backend/supabase.json', 'r') as file:
+    config = json.load(file)
+url: str = config.get("SUPABASE_URL")
+key: str = config.get("SUPABASE_SECRET")
+supabase: Client = create_client(url, key)
+
+def pushAlert(image: str):
+    time = datetime.now().strftime('%Y%m-%d%H-%M%S')
+    uid = f"alert_{time}" # Generate Unique Identifier of Alert based on the datetime it is created
+    imagePath = f"public/alert_{uid}"
+    rw.write(supabase=supabase, table="Alerts", uid=uid, is_resolved=False, image_path=imagePath)
+    resp = supabase.storage.from_("AlertImages").upload(path=imagePath, file=image, file_options={"cache-control": "3600", "upsert": "false", "content-type":"image/jpeg"})
+    print(resp)
+
+pushAlert("RasPi/6143325340081178130.jpg")
