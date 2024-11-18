9749daf1a3aa3d82efeef88ed01136413ab76f25
Paun Wander <tsme0613@gmail.com>
2024-11-18 23:11:15 +0800

    1

--- scripts/utils.py
+++ scripts/utils.py
@@ -39,7 +39,7 @@ def capture():
 
 def updateHandshake():
     # Generate the current time in the desired format
-    response = supabase.table("handshake").select("*").eq("id", 1).execute()
+    response = supabase.table("handshake").select("*").eq("id", 1).execute().json()
     last_time = response['data'][0]['last_time']
 
     timestamp = datetime.fromisoformat(last_time)
